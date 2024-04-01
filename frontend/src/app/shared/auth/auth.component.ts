4a528fa143d1de2e1d9f04e1092101236ff2ca28
Brooklyn Sides <brooklynjsides@gmail.com>
2024-04-01 17:59:16 -0500

    Added login page, nonfunctional

--- frontend/src/app/shared/auth/auth.component.ts
+++ frontend/src/app/shared/auth/auth.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
