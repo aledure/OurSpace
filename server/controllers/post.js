09738d2dcb0f2c612a2bc5a6ea84705a0a921d29
Chassidy90 <Chassidykay90@gmail.com>
2024-04-11 19:33:50 -0500

    like

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,10 +1,11 @@
-const Post = require('./../models/post.model');
+const Post = require("./../models/post.model");
 
 // Create a new post
 const createPost = async (req, res) => {
   try {
     const { title, content, image, createdBy } = req.body;
-    const post = await Post.create({ title, content, image, createdBy });
+    console.log('creating post')
+    const post = await Post.create({ title, content });
     console.log("New post created with ID:", post._id); // Log the post's ID
     res.status(201).json({ success: true, data: post });
   } catch (error) {
@@ -12,15 +13,18 @@ const createPost = async (req, res) => {
   }
 };
 
-
 // Edit a post
 const editPost = async (req, res) => {
   try {
     const { id } = req.params;
     const { title, content, image } = req.body;
-    const post = await Post.findByIdAndUpdate(id, { title, content, image }, { new: true });
+    const post = await Post.findByIdAndUpdate(
+      id,
+      { title, content, image },
+      { new: true }
+    );
     if (!post) {
-      return res.status(404).json({ success: false, error: 'Post not found' });
+      return res.status(404).json({ success: false, error: "Post not found" });
     }
     res.status(200).json({ success: true, data: post });
   } catch (error) {
@@ -34,7 +38,7 @@ const deletePost = async (req, res) => {
     const { id } = req.params;
     const post = await Post.findByIdAndDelete(id);
     if (!post) {
-      return res.status(404).json({ success: false, error: 'Post not found' });
+      return res.status(404).json({ success: false, error: "Post not found" });
     }
     res.status(200).json({ success: true, data: {} });
   } catch (error) {
@@ -48,7 +52,7 @@ const getPostById = async (req, res) => {
     const { id } = req.params;
     const post = await Post.findById(id);
     if (!post) {
-      return res.status(404).json({ success: false, error: 'Post not found' });
+      return res.status(404).json({ success: false, error: "Post not found" });
     }
     res.status(200).json({ success: true, data: post });
   } catch (error) {
@@ -77,4 +81,36 @@ const getAllPosts = async (req, res) => {
   }
 };
 
-module.exports = { createPost, editPost, deletePost, getPostById, getPostsByUser, getAllPosts };
+//Like A Post
+
+const likePost = async (req, res) => {
+  try {
+    // const {userId} = req.user
+    const userId = 'asdfasdfasdfs'
+    const id = req.params.id
+    if (!id) {
+      res.status(400).json({ success: false, error: 'Server Error' });
+    }
+    const post = await Post.findById(id);
+
+    //Increase likes
+    post.like += 1;
+    // post.usersLiked.push(userId);
+
+    await post.save();
+
+    res.status(200).json({ success: true, message: `Post Liked!` });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+module.exports = {
+  createPost,
+  editPost,
+  deletePost,
+  getPostById,
+  getPostsByUser,
+  getAllPosts,
+  likePost,
+};
