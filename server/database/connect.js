6bb9deb6403344bc57e026cf181708147f3b870c
aledure <63022012+aledure@users.noreply.github.com>
2024-04-18 19:42:09 -0500

    Update connect.js

--- server/database/connect.js
+++ server/database/connect.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {});
+    await mongoose.connect(process.env.MONGODB_URL, {});
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB:", error);
