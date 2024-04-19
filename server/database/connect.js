148c568aecc8b22508e6c3c6444cb770b25e2c2f
jmdarnell90 <128856646+jmdarnell90@users.noreply.github.com>
2024-04-18 19:05:52 -0500

    ongo fixed

--- server/database/connect.js
+++ server/database/connect.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
+        await mongoose.connect(process.env.MONGO_URL, {
         });
         console.log('Connected to MongoDB');
     } catch (error) {
